69fde2f125e75ec75d7125100aa7029408e612a1
Vitalijus Siaurys <vitalijus.siaurys@gmail.com>
2025-11-25 10:50:19 +0200

    Apply [JsonPropertyOrder] attributes to the properties of the [Employee](JsonSerialization/Employee.cs) class

--- JsonSerialization/Employee.cs
+++ JsonSerialization/Employee.cs
@@ -5,14 +5,18 @@ namespace JsonSerialization;
 public class Employee
 {
     [JsonPropertyName("employee_name")]
+    [JsonPropertyOrder(4)]
     public string? Name { get; set; }
 
     [JsonPropertyName("employee_age")]
+    [JsonPropertyOrder(3)]
     public int Age { get; set; }
 
     [JsonPropertyName("employee_role")]
+    [JsonPropertyOrder(2)]
     public string? Role { get; set; }
 
     [JsonPropertyName("employee_id")]
+    [JsonPropertyOrder(1)]
     public int Id { get; set; }
 }
