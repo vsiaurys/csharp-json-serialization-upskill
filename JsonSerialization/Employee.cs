1f33b6c7c4f23b2e85f5ec2aa1edd97ae8c1a1f1
Vitalijus Siaurys <vitalijus.siaurys@gmail.com>
2025-11-25 10:49:02 +0200

    Apply [JsonPropertyName] attributes to the properties of the [Employee](JsonSerialization/Employee.cs) class

--- JsonSerialization/Employee.cs
+++ JsonSerialization/Employee.cs
@@ -4,11 +4,15 @@ namespace JsonSerialization;
 
 public class Employee
 {
+    [JsonPropertyName("employee_name")]
     public string? Name { get; set; }
 
+    [JsonPropertyName("employee_age")]
     public int Age { get; set; }
 
+    [JsonPropertyName("employee_role")]
     public string? Role { get; set; }
 
+    [JsonPropertyName("employee_id")]
     public int Id { get; set; }
 }
